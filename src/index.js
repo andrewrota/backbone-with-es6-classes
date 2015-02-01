4aa2bdf8eb863727c75f08bacc1fe4d0b3411e64
andrewrota <rota.andrew@gmail.com>
2015-02-01 18:59:38 -0500

    Remove superfluous jQuery import, and remove console.log

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 'use strict';
 import backbone from 'backbone';
-import $ from 'jquery';
-console.log(backbone);
-export class Hello extends backbone.View {
+
+class Hello extends backbone.View {
     render() {
         this.$el.html('Hello, world.');
     }
