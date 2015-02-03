89f52683ac13863e6f37055642988897e9868712
andrewrota <rota.andrew@gmail.com>
2015-02-03 00:10:26 +0000

    Add model class with method.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 'use strict';
 import backbone from 'backbone';
 
+class Person extends backbone.Model {
+    getFullName() {
+        return this.get('firstName') + ' ' + this.get('lastName');
+    }
+}
+
 class Hello extends backbone.View {
+    initialize() {
+        this.person = new Person({
+            firstName: 'George',
+            lastName: 'Washington'
+        });
+    }
     render() {
-        this.$el.html('Hello, world.');
+        this.$el.html('Hello, ' + this.person.getFullName() + '.');
     }
 }
 
