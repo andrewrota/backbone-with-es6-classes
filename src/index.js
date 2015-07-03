e94bece363084fb0573355b5ec9fa4a799a4f833
Andrew Rota <rota.andrew@gmail.com>
2015-07-03 16:17:35 -0400

    Add decorator for properties on class

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 'use strict';
 import backbone from 'backbone';
+import _ from 'underscore';
 
 class Person extends backbone.Model {
     getFullName() {
         return this.get('firstName') + ' ' + this.get('lastName');
     }
 }
-
+// Using benmccormick's suggestion
+// for decorators to apply properties to class
+// before instantiation
+// https://github.com/jashkenas/backbone/issues/3560#issuecomment-113709224
+function props(value) {
+    return function decorator(target) {
+        _.extend(target.prototype, value);
+    }
+}
+@props({
+    events: {
+        'click': function() { console.log('clicked!'); }
+    }
+})
 class Hello extends backbone.View {
     initialize() {
         this.person = new Person({
